56b19a3e01345c55054f60c025e3e2e1220c896c
shibichris <99465215+shibichris@users.noreply.github.com>
2022-07-06 12:22:15 +0530

    Model

---
+++ Book.java
@@ -0,0 +1,41 @@
+package model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection ="Book")
+public class Book {
+     
+	@Id
+	private int id;
+	private String bookName;
+	private String authorName;
+	
+	public Book() {
+		
+	}
+	public Book(int id, String bookName, String authorName) {
+		
+		this.id = id;
+		this.bookName = bookName;
+		this.authorName = authorName;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	public String getAuthorName() {
+		return authorName;
+	}
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+}
